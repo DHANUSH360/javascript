7265b1b0e50247fcce98b6734020a37d74ddd19d
DHANUSH360 <dhanushponnada360@gmail.com>
2026-05-06 10:18:08 +0530

    Counter app using javascript

---
+++ Day2/script.js
@@ -0,0 +1,34 @@
+function onIncrement()
+{   
+    console.log('onIncrement function is triggered')
+    let count_ele=document.getElementById('count')
+    let number=parseInt(count_ele.textContent)
+    count_ele.textContent=number+1
+    let value=parseInt(count_ele.textContent)
+    if (value>0){
+        count_ele.style.color='green'
+    }
+    else if(value<0){
+        count_ele.style.color='red'
+    }
+    else{
+        count_ele.style.color='black'
+    }
+}
+function onDecrement()
+{
+    console.log('onDecrement function is triggered')
+    let count_ele=document.getElementById('count')
+    let number=parseInt(count_ele.textContent)
+    count_ele.textContent=number-1
+    let value=parseInt(count_ele.textContent)
+    if (value>0){
+        count_ele.style.color='green'
+    }
+    else if(value<0){
+        count_ele.style.color='red'
+    }
+    else{
+        count_ele.style.color='black'
+    }
+}
