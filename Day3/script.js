d37d279afa4426b5c701f00f19b1ea508095076e
DHANUSH360 <dhanushponnada360@gmail.com>
2026-05-07 11:14:43 +0530

    color picker task

---
+++ Day3/script.js
@@ -0,0 +1,22 @@
+let btn1=document.getElementById('green_btn')
+let btn2=document.getElementById('yellow_btn')
+let btn3=document.getElementById('purple_btn')
+let btn4=document.getElementById('pink_btn')
+let arr=['green','yellow','purple','pink']
+btn1.style.backgroundColor=arr[0]
+btn2.style.backgroundColor=arr[1]
+btn3.style.backgroundColor=arr[2]
+btn4.style.backgroundColor=arr[3]
+function all_in_one(color){
+    document.body.style.backgroundColor=color
+}
+function all_in_one(color){
+    document.body.style.backgroundColor=color
+}
+function all_in_one(color){
+    document.body.style.backgroundColor=color
+}
+function all_in_one(color){
+    document.body.style.backgroundColor=color
+}
+
