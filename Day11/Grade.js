ecfd8c5faa22cf38a7decbdaae82ac3143b8a598
DHANUSH360 <dhanushponnada360@gmail.com>
2026-05-31 10:07:37 +0530

    Grade Display Program

---
+++ Day11/Grade.js
@@ -0,0 +1,21 @@
+//To display grades for students by using conditional statements.
+let marks = 92;
+
+if (marks >= 90) {
+    console.log("Grade: A+");
+}
+else if (marks >= 80) {
+    console.log("Grade: A");
+}
+else if (marks >= 70) {
+    console.log("Grade: B");
+}
+else if (marks >= 60) {
+    console.log("Grade: C");
+}
+else if (marks >= 35) {
+    console.log("Grade: D");
+}
+else {
+    console.log("Fail");
+}
